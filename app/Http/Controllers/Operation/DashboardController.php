08efa2726328f22e4981ff7592a7b147b6e63bf2
LauraLuna3 <desarrollo3.starpark@gmail.com>
2025-05-23 11:34:59 -0500

    Dashboard users active

---
+++ app/Http/Controllers/Operation/DashboardController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Operation;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function countActiveUsers()
+    {
+        $count_user = User::where('active', 1)->count();
+
+        return $this->responseLivewire('success', 'succes', $count_user);
+    }
+}
