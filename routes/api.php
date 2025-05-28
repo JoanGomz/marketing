93c43804ec45158c192e455609eded65c57e557e
LauraLuna3 <desarrollo3.starpark@gmail.com>
2025-05-28 14:21:18 -0500

    settings redirect roll

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,14 @@
 <?php
 
 use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\Operation\DashboardController;
 use App\Http\Controllers\Operation\ParksController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/test', function (Request $request) {});
 
-Route::get('/park', [UserController::class, 'index']);
+Route::get('/park', [DashboardController::class, 'countActiveUsers']);
 Route::post('/park', [UserController::class, 'store']);
 Route::put('/park/{id}', [UserController::class, 'update']);
 Route::delete('/park/{id}', [UserController::class, 'destroy']);
